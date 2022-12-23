a22098a14f681f227c0ab87d71ee090a74f75157
jeremy302 <olajeremy123@gmail.com>
2022-12-23 01:04:50 +0100

    patch

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,13 +10,4 @@ def do_pack():
     ''' <TODO> doc for code '''
     os.path.isdir("versions") or os.mkdir("versions")
     now = datetime.now()
-    filename = (f"versions/web_static_{now.year}{now.month}{now.day}" +
-                f"{now.hour}{now.minute}{now.second}.tgz")
-    try:
-        print(f"Packing web_static to {filename}")
-        local(f"tar -fczv {filename} web_static")
-        size = os.path.getsize(filename)
-        print(f"web_static packed: {filename} -> {size} Bytes")
-    except Exception:
-        return None
-    return filename
+    return None
