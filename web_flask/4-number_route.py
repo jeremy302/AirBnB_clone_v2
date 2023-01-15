0a0bf14bbcc88fc39580a4c8ce0f6eec14250737
jeremy302 <olajeremy123@gmail.com>
2023-01-15 18:42:46 +0100

    patch

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+''' <TODO> add documentation '''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    ''' <TODO> add documentation '''
+    return 'Hello HBNB'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    ''' <TODO> add documentation '''
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_fun(text):
+    ''' <TODO> add documentation '''
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', defaults={'text': 'is cool'},  strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_cool(text):
+    ''' <TODO> add documentation '''
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def number(n):
+    ''' <TODO> add documentation '''
+    if n.isdigit():
+        return '{} is a number'.format(n)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
