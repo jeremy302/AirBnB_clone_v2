f9cca15229279d087330d95dedadb8919c1278be
jeremy302 <olajeremy123@gmail.com>
2023-01-15 18:31:21 +0100

    patch

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,7 +19,7 @@ def hbnb():
 @app.route('/c/<text>', strict_slashes=False)
 def py_cool(text):
     ''' <TODO> add documentation '''
-    return f"C {text.replace('_', ' ')}"
+    return 'C {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
