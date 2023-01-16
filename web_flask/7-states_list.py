f8a044ef38f6026cb4d5e0c93011b5f5536ba297
jeremy302 <olajeremy123@gmail.com>
2023-01-16 03:21:30 +0100

    patch

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 ''' <TODO> add documentation '''
 from flask import Flask, render_template
-from models import storage
-from models.state import State
+from ..models import storage
+# from models.state import State
 
 app = Flask(__name__)
 
